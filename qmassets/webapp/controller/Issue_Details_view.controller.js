58a6e7a5fddb2cc6c02b2d6567ba7d282f477739
Ahmed Gharbi <agharbi@deloitte.tn>
2024-08-14 13:20:46 +0000

    add section fragments

---
+++ qmassets/webapp/controller/Issue_Details_view.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("qmassets.controller.Issue_Details_view", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ qmassets/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"qmassets/test/unit/controller/Issue_Details_view.controller"
+], function () {
+	"use strict";
+});
